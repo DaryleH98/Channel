97bb3bed0021196edf84b3ec00190fd001d032e9
D4ryl3ne1 <Darylehen02@gmail.com>
2019-12-30 22:03:32 -0500

    Added EventListener for each namespace object to fill UI Dynamically

--- public/scripts.js
+++ public/scripts.js
@@ -5,9 +5,23 @@ socket.on('connect', ()=>{
     console.log(socket.id)
 })
 
+//Listen for a list of namespaces
 socket.on('nsList', (nsData)=>{
     console.log("The list of namespaces has arrived!")
+    let namespacesDiv = document.querySelector('.namespaces')
+    namespacesDiv.innerHTML = ""
+    nsData.forEach((ns)=>{
+        namespacesDiv.innerHTML +=  `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}"/></div>`
+    })
+    //Add a click listener for each NS
+    Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
+        elem.addEventListener('click',(e)=>{
+            const nsEndpoint = elem.getAttribute('ns')
+            console.log(`${nsEndpoint}`)
+        })
+    })
 })
+
 socket.on('messageFromServer', (dataFromServer)=>{
     console.log(dataFromServer);
     socket.emit('dataToServer', {data: "Data from the Client!"})
